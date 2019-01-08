5e87d4d0a1a77573c90e19e25c45d512b2c94333
Libor <liborpansky@users.noreply.github.com>
2019-01-08 06:53:10 +0100

    VueCliMiddleware updated to 2.2.0 with updated regex, fixes #3

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace AspNetCoreVueStarter
                 if (env.IsDevelopment())
                 {
                     // run npm process with client app
-                    spa.UseVueCli(npmScript: "serve", port: 8080);
+                    spa.UseVueCli(npmScript: "serve", port: 8080, regex: "Compiled ");
                     // if you just prefer to proxy requests from client app, use proxy to SPA dev server instead:
                     // app should be already running before starting a .NET client
                     // spa.UseProxyToSpaDevelopmentServer("http://localhost:8080"); // your Vue app port
