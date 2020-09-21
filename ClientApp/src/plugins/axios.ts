aff237c990132cbdd529d05c86f600c243bc3270
Libor <liborpansky@users.noreply.github.com>
2020-09-21 07:02:53 +0200

    #39 Fix tslint warnings and Vue interface case

--- ClientApp/src/plugins/axios.ts
+++ ClientApp/src/plugins/axios.ts
@@ -1,55 +1,47 @@
-"use strict";
+'use strict';
 
-import vue from 'vue';
-import axios, { AxiosInstance } from "axios";
+import Vue from 'vue';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-let config = {
+const config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
 
+// tslint:disable-next-line: variable-name
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+  // Do something before request is sent
+  (conf: AxiosRequestConfig) => conf,
+  // Do something with request error
+  (error: any) => Promise.reject(error),
 );
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
+  // Do something before request is sent
+  (response: any) => response,
+  // Do something with response error
+  (error: any) => Promise.reject(error),
 );
 
-function AxiosPlugin(Vue: typeof vue, options?: any): void {
-    Vue.prototype.$axios = _axios;
+function AxiosPlugin(vue: typeof Vue, options?: any): void {
+  vue.prototype.$axios = _axios;
 }
 
 declare module 'vue/types/vue' {
-    interface Vue {
-        $axios: AxiosInstance;
-    }
+  interface Vue {
+    $axios: AxiosInstance;
+  }
 }
 
-vue.use(AxiosPlugin)
-
+Vue.use(AxiosPlugin);
 
 export default Plugin;
