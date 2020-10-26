873d50ff63f517ab48b23c3116127ca0d33cfbd5
Tobias Lønnerød Madsen <m@dsen.tv>
2020-10-26 20:47:20 +0100

    Enabling HTTPS by default for development

--- Startup.cs
+++ Startup.cs
@@ -44,9 +44,10 @@ namespace AspNetCoreVueStarter
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHttpsRedirection();
             }
 
+            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
